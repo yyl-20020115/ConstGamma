e8c9879dd4bc4013ab449dddb8c908216ba4b605
Yilin Yang <yyl_20050115@hotmail.com>
2025-01-29 01:48:15 +0800

    fx

--- ConstGamma/Program.cs
+++ ConstGamma/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using ExtendedNumerics;
+using System.Numerics;
 
 namespace ConstGamma;
 
@@ -133,13 +134,21 @@ public class Program
         }
         return sum;
     }
+    static double HalfPi1(double n)
+    {
+        return 1 + Math.Pow(n / (2.0 * n + 1.0), n);
+    }
+    static double HalfPi2(double n)
+    {
+        return Math.Pow(1.0 + 1.0 / (2.0 + 1.0 / n), 1.0 / n);
+    }
 
     static Complex ZetaComplex(double b, double error)
     {
         var sum = Complex.One;
         var bp = b * Math.PI;
         var s = new Complex(0.5, bp);
-        for (var i = 1.0; ; i++)
+        for (var i = 2.0; ; i++)
         {
             var v = Complex.Pow(i, -s);
             if (v.Magnitude < error)
@@ -150,51 +159,137 @@ public class Program
         }
         return sum;
     }
+
+    static double ToDouble(BigDecimal value)
+    {
+        //Eps=4.94065645841247E-324;
+        //Max=1.7976931348623157E+308
+        var t = BigDecimal.ToScientificENotation(value);
+
+        return double.TryParse(t, out var v) ? v : 0;
+    }
+    static double GetAbsSquare(BigComplex bigComplex)
+    {
+        var r = ToDouble(bigComplex.Real);
+        var i = ToDouble(bigComplex.Imaginary);
+        
+        return r * r + i * i;
+    }
+
+    static BigComplex ZetaBigComplex(double b, double error = 5e-3)
+    {
+        var sum = BigComplex.One;
+        var bp = b * Math.PI;
+        var s = new BigComplex(0.5, bp);
+        for (var i = 2.0; ; i++)
+        {
+            var v = BigComplex.Pow(i, -s);
+            var d = GetAbsSquare(v);
+            if (d < error)
+            {
+                break;
+            }
+            sum += v;
+        }
+        return sum;
+    }
+
+
     static string GetSign(double d) => d >= 0 ? "+" : "";
 
     class ComplexComparer : IComparer<(Complex, double)>
     {
         public int Compare((Complex, double) x, (Complex, double) y)
         {
-            return Math.Abs(x.Item1.Real)>=Math.Abs(y.Item1.Real)?1:0;
+            var d = Math.Abs(x.Item1.Real) - Math.Abs(y.Item1.Real);
+
+            return d > 0 ? 1 : d < 0 ? -1 : 0;
         }
     }
-    static bool GetZetaResults(double min = -16, long steps = 1600, double error = 5e-3)
+    static bool GetZetaResults(double min = -16, long steps = 16000, double error = 5e-3)
     {
-        var ms = new List<(Complex c,double b)>();
+        var ms = new List<(Complex c, double b)>();
         for (double b = 0.0; b >= min; b += min / steps)
         {
             var n = ZetaComplex(b, error);
             var z = n / n.Magnitude;
-
-            var m = Math.Abs(n.Real) < Math.Abs(z.Real) ? n : z;
-            ms.Add((m,b));
+            ms.Add((z, b));
         }
 
         ms.Sort(new ComplexComparer());
+
+        using var writer = new StreamWriter("zeta.txt");
+
+
+        for (int i = 0; i < NontrivalZeros.Length; i++)
+        {
+            var n = NontrivalZeros[i];
+            var b = n.Imaginary / Math.PI;
+            var t = ms.MinBy(m => Math.Abs(m.b - b));
+            var m = t.c;
+
+            var nv = ZetaComplex(b, error);
+            nv /= nv.Magnitude;
+            writer.WriteLine($"NontrivalZeros[{i}] = {NontrivalZeros[i]}, Imaginary/Pi={NontrivalZeros[i].Imaginary / Math.PI} : {GetSign(m.Real)}{m.Real:N8}{GetSign(m.Imaginary)}{m.Imaginary:N8}i, b={GetSign(t.b)}{t.b:N8},{GetSign(m.Real)}{m.Real:N8}{GetSign(m.Imaginary)}{m.Imaginary:N8}i, b={GetSign(t.b)}{t.b:N8}, nv={GetSign(nv.Real)}{nv.Real:N8}{GetSign(nv.Imaginary)}{nv.Imaginary:N8}i");
+        }
+
         for (int i = 0; i < ms.Count; i++)
         {
             var m = ms[i].c;
             var b = ms[i].b;
-            if (Math.Abs(m.Real) < 1e-2)
-            {
-                Console.WriteLine($"T:ZetaZero = {GetSign(m.Real)}{m.Real:N8}{GetSign(m.Imaginary)}{m.Imaginary:N8}i, b={GetSign(b)}{b:N8}");
-            }
-            else
-            {
-                Console.WriteLine($"F:ZetaZero = {GetSign(m.Real)}{m.Real:N8}{GetSign(m.Imaginary)}{m.Imaginary:N8}i, b={GetSign(b)}{b:N8}");
-            }
+            writer.WriteLine($"T:ZetaZero = {GetSign(m.Real)}{m.Real:N8}{GetSign(m.Imaginary)}{m.Imaginary:N8}i, b={GetSign(b)}{b:N8}");
         }
 
+
         return true;
+    }
+
+    //var p_2 = (Math.Pow((2.0 + Math.E), -1.0 / (Math.E)) + 1.0);
+    //var d = p_2 - Math.PI / 2.0;
+    //Console.WriteLine($"p0={Math.PI/2.0} p={p_2},d={d}, r={Math.Abs(2*d/Math.PI)}");
+    //return;
+    static double GetElipticE(double axial_ratio)
+        => Math.Sqrt(1.0 - axial_ratio * axial_ratio);
+    static decimal GetPi(long n, decimal axial_ratio = 1.0m) //axial_ratio 轴比
+    {
+
+        decimal numerator = 1.0m;
+        decimal denominator = 1.0m;
+        decimal sum = 1.0m;
+
+        for (long i = 1; i <= n; i++)
+        {
+            numerator *= i * axial_ratio;
+            denominator *= (2.0m * i * axial_ratio + 1.0m);
+            sum += numerator / denominator;
+        }
+
+        return 2.0m * sum;
     }
     static void Main(string[] args)
     {
         ArgumentNullException.ThrowIfNull(args);
-        for (int i = 0; i < NontrivalZeros.Length; i++)
+
+
+        var b = NontrivalZeros[0].Imaginary;
+
+        var z = ZetaBigComplex(b);
+        var r = z.Magnitude;
+        var rx = z.Real / r;
+        var ri = z.Imaginary / r;
+        Console.WriteLine($"ZetaBigComplex({b}) = {rx} + {ri}i, r={r}");
+
+        var pi = GetPi(22, 1.0m);
+        Console.WriteLine($"Pi={pi}");
+
+        for (double c = 1; c <= 1000; c++)
         {
-            Console.WriteLine($"NontrivalZeros[{i}] = {NontrivalZeros[i]}, Imaginary/Pi={NontrivalZeros[i].Imaginary / Math.PI}");
+            double h1 = HalfPi1(c);
+            double h2 = HalfPi2(c);
+            double m = (h1 + h2) / 2.0;
+            Console.WriteLine($"c={c}, m={m} HalfPi1={h1}, HalfPi2={h2}, diff={h1 - h2}");
         }
+
 
         GetZetaResults();
 
